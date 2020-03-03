418961615ef7072442958b6a3ba996f176fec879
Tomasz Stepnik <tomasz.stepnik77@gmail.com>
2020-03-03 16:23:56 +0100

    resolve counting problem, add methods for filter and print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
         MegaMillions mega = new MegaMillions();
-        mega.getUniqueWinningNumbersWithOccur().forEach(System.out::println);
+        mega.filterAndPrintNumbers();
     }
 }
--- src/main/java/MegaMillions.java
+++ src/main/java/MegaMillions.java
@@ -10,43 +10,57 @@ public class MegaMillions {
 
     File file = new File("src/main/resources/mega_millions.csv");
     String absolutePath = file.getAbsolutePath();
-    private List<Integer> allWinningNumbers = getAllWinningNumbers();
-    private List<Number> uniqueWinningNumbersWithOccur = getUniqueNumbersWithOccurs(allWinningNumbers);
 
-    public List<Number> getUniqueWinningNumbersWithOccur() {
-        return uniqueWinningNumbersWithOccur;
+    public void filterAndPrintNumbers() {
+        List<Integer> allWinningNumbers = new ArrayList<>();
+        List<Integer> allWinningMegaBall = new ArrayList<>();
+        getAllWinningNumbers(allWinningNumbers, allWinningMegaBall);
+        List<Number> winning75Numbers = getUniqueNumbersWithOccurs(allWinningNumbers);
+        List<Number> winningMegaBalls = getUniqueNumbersWithOccurs(allWinningMegaBall);
+        System.out.println("Top 10 winning numbers:");
+        mostPopularNumberFromList(winning75Numbers, 10);
+        System.out.println("Top 3 winning mega ball:");
+        mostPopularNumberFromList(winningMegaBalls, 3);
     }
 
-    private List<Integer> getAllWinningNumbers() {
+    private void getAllWinningNumbers(List<Integer> winningNumbers, List<Integer> megaBalls) {
         String line = "";
-        List<Integer> winningNumbers = new ArrayList<>();
         try (
                 var fileReader = new FileReader(absolutePath);
                 var bufferedReader = new BufferedReader(fileReader);
         ) {
-            while (bufferedReader.readLine() != null) {
-                line = bufferedReader.readLine();
-                addNumbersFromLineToList(line, winningNumbers);
+            while ((line = bufferedReader.readLine()) != null) {
+                addWinningNumbers(line, winningNumbers);
+                addMegaBalls(line,megaBalls);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return winningNumbers;
     }
 
-    public void addNumbersFromLineToList(String line, List<Integer> winningNumbersList) {
-        String[] splitedByComma = line.split(",");
-        String[] numbers = splitedByComma[1].split(" ");
+    public void addWinningNumbers(String line, List<Integer> winningNumbersList) {
+        String[] splitByComma = line.split(",");
+        String[] numbers = splitByComma[1].split(" ");
         for (String number : numbers) {
-            System.out.print(number + " ");
-            winningNumbersList.add(Integer.parseInt(number));
+            if (number.matches("-?\\d+")) {
+                winningNumbersList.add(Integer.parseInt(number));
+            }
         }
-        System.out.println();
+    }
+
+    public void addMegaBalls(String line, List<Integer> megaBalls){
+        String[] splitByComma = line.split(",");
+        String[] megaBall = splitByComma[2].split(" ");
+        for (String ball : megaBall) {
+            if (ball.matches("-?\\d+")) {
+                megaBalls.add(Integer.parseInt(ball));
+            }
+        }
     }
 
     private List<Number> getUniqueNumbersWithOccurs(List<Integer> list) {
         List<Number> numbersWithOccurs = new ArrayList<>();
-        for (int i = 1; i <= 75; i++) {
+        for (int i = 1; i <= 10000; i++) {
             int occurs = 0;
             for (int j = 0; j < list.size(); j++) {
                 if (list.get(j) == i) {
@@ -58,13 +72,12 @@ public class MegaMillions {
         return numbersWithOccurs;
     }
 
-    //    i did this method according to Task but still cant't figure out how to count numbers properly.gi
-    public void tenMostPopularNumberFromList(List<Number> list) {
-        System.out.println("10 najpopularniejszych liczb:");
-        list.sort(Comparator.comparingInt(Number::getNumber).reversed());
-        for (int i = 0; i < list.size(); i++) {
-            String info = list.get(i).toString();
+    public void mostPopularNumberFromList(List<Number> numbers, int howManyPrint) {
+        numbers.sort(Comparator.comparingInt(Number::getHowManyTimesOccurs).reversed());
+        for (int i = 0; i < howManyPrint; i++) {
+            String info = numbers.get(i).toString();
             System.out.println(info);
         }
+        System.out.println();
     }
 }
